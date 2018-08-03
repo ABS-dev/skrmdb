440bf69781da5729ff1d438a86405a286eeb641d
Marie Vendettuoli <mariecv26@gmail.com>
2018-08-03 11:00:24 -0500

    roll version & date forward, add help links

--- R/skrmdb-package.r
+++ R/skrmdb-package.r
@@ -6,14 +6,23 @@
 #' \tabular{ll}{
 #' Package: \tab skrmdb-package\cr
 #' Type: \tab Package\cr
-#' Version: \tab 4.2.4\cr
-#' Date: \tab 2014-01-28\cr
+#' Version: \tab 4.2.5\cr
+#' Date: \tab 2018-08-03\cr
 #' License: \tab MIT \cr
 #' LazyLoad: \tab yes\cr
 #' LazyData: \tab yes\cr
 #' }
 #'
 #' @name skrmdb-package
+#' @aliases skrmdb
 #' @docType package
 #' @author David Siev \email{David.Siev@@aphis.usda.gov}
+#' @section Resources:
+#' 
+#' \itemize{
+#' \item QUICK START: \url{https://github.com/ABS-dev/skrmdb/blob/master/README.md}
+#' \item PACKAGE VIGNETTE: \url{https://www.aphis.usda.gov/animal_health/vet_biologics/publications/STATWI0001.pdf}
+#' \item BUG REPORTS: \url{https://github.com/ABS-dev/skrmdb/issues}
+#' }
+#' 
 NULL
