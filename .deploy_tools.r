3425124e9127dbaf36196c9e7542a0933e9e4015
Thomas Kent <thomas.kent@usda.gov>
2024-06-27 17:12:38 -0500

    Update Roxygen formatting

---
+++ .deploy_tools.r
@@ -0,0 +1,71 @@
+## Create Static Documentation ####
+
+# 1. DESCRIPTION should contain this line: `Config/build/clean-inst-doc: FALSE`
+# 2. Should be run from within root directory of project.
+# 3. Roll version forward, install, but do not push
+# 4. Install
+# 5. Run this script
+# 6. Update tags and push
+# 7. Push new version
+CVBdevtools::create_static_documentation()
+
+
+## Create pkgdown Content locally ####
+
+CVBdevtools::deploy_pkgdown_local()
+
+# I ran git update-index --assume-unchanged <file> in the terminal for each file
+# in pkgdown and public to remove them from being followed.  Hopefully it
+# sticks.
+#
+# I finally just started deploying into docs/ since that has been in gitigore
+# since the beginning.
+
+## Deploy pkgdown Content to remote git repository ####
+
+# 1. Roll Version forward, install and then run this script.
+CVBdevtools::deploy_pkgdown_remote()
+
+# Lint Package with Extended Version
+
+{
+  library(data.table)
+
+  # Lint the package to check for formatting errors
+  # Path should be the root of the package directory.
+  path <- getwd()
+  linter <- N <- NULL
+
+  # Variables that don't follow stylistic guidelines, which we are not changing.
+
+  okay_vars <- c("")
+
+  ## Change This Value ##
+  # Determine whose files should be linted.  Change the number in the [.]
+
+  excluded_files <- included_files <- NULL
+
+  idx <- 1
+  excluded_linters <- c("indentation_linter",
+                        "cyclocomp_linter",
+                        "commented_code_linter",
+                        "object_usage_linter"
+  )[idx]
+
+  # Run lintr on the selected files.
+  tmp <- codeDiagnostics::lint_package_extended(
+    path = path,
+    okay_vars = okay_vars,
+    excluded_files = excluded_files,
+    included_files = included_files,
+    excluded_linters = excluded_linters)
+
+  # Send results to Markers screen
+  if (length(tmp) == 0) {
+    message("Sucess!  No lintr issues!")
+  } else {
+    tmp
+  }
+}
+
+data.table(tmp)
--- R/skrmdb.r
+++ R/skrmdb.r
@@ -32,19 +32,26 @@
 #'   as the Reed-Muench method.
 
 #' @references Behrens, B. (1929) Zur Auswertung der Digitalisblätter im
-#'   Froschversuch. \emph{Arkiv für Experimentelle Pathologie und
-#'   Pharmakologie.} \bold{140: 237-256}.\cr Dragstedt, C. A., Lang, V. F.
-#'   (1928). Respiratory Stimulants in Acute Cocaine Poisoning in Rabbits.
-#'   \emph{J. of Pharmacology and Experimental Therapeutics.} \bold{32:
-#'   215--222}.\cr Kärber, G. (1931). Beitrag zur kollektiven Behandlung
-#'   Parmakogischer Reihenversuche. \emph{Archiv für Experimentelle Pathologie
-#'   und Pharmakologie.} \bold{162: 480--483}.\cr Miller, Rupert G. (1973).
-#'   Nonparametric Estimators of the Mean Tolerance in Bioassay.
-#'   \emph{Biometrika.} \bold{60: 535 - 542}.\cr Reed LJ, Muench H (1938). A
-#'   Simple Method of Estimating Fifty Percent Endpoints. \emph{American Journal
-#'   of Hygiene.} \bold{27: 493--497.} \cr Spearman, C. (1908). The Method of
-#'   'Right and Wrong Cases' ('Constant Stimuli') without Gauss's Formulae.
-#'   \emph{Brit. J. of Psychology.} \bold{2: 227--242.}
+#'   Froschversuch. *Arkiv für Experimentelle Pathologie und Pharmakologie.*
+#'   **140: 237-256**.
+#'
+#'   Dragstedt, C. A., Lang, V. F. (1928). Respiratory Stimulants in Acute
+#'   Cocaine Poisoning in Rabbits. *J. of Pharmacology and Experimental
+#'   Therapeutics.* **32: 215--222**.
+#'
+#'   Kärber, G. (1931). Beitrag zur kollektiven Behandlung Parmakogischer
+#'   Reihenversuche. *Archiv für Experimentelle Pathologie und Pharmakologie.*
+#'   **162: 480--483**.
+#'
+#'   Miller, Rupert G. (1973). Nonparametric Estimators of the Mean Tolerance in
+#'   Bioassay. *Biometrika.* **60: 535 - 542**.
+#'
+#'   Reed LJ, Muench H (1938). A Simple Method of Estimating Fifty Percent
+#'   Endpoints. *American Journal of Hygiene.* **27: 493--497**.
+#'
+#'   Spearman, C. (1908). The Method of 'Right and Wrong Cases' ('Constant
+#'   Stimuli') without Gauss's Formulae. *Brit. J. of Psychology.* **2:
+#'   227--242**.
 #'
 #' @param formula a formula of the form `y + n ~ x` or `cbind(y, n) ~ x`
 #' @param data a data frame
